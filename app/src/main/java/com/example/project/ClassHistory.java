3830a84a5abf05fa08da8d53002e5f011821c946
bjarkividars <bjarki.vidars@gmail.com>
2022-02-06 18:08:30 -0800

    class history fixed

--- app/src/main/java/com/example/project/ClassHistory.java
+++ app/src/main/java/com/example/project/ClassHistory.java
@@ -5,9 +5,11 @@ import static android.content.ContentValues.TAG;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteConstraintException;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.project.model.AppDatabase;
@@ -25,18 +27,39 @@ public class ClassHistory extends AppCompatActivity {
         setContentView(R.layout.activity_class_history);
     }
 
-    public void onSaveHistoryButtonClicked(View view){
-        TextView yearView = findViewById(R.id.editTextYear);
-        TextView quarterView = findViewById(R.id.editTextQuarter);
-        TextView subjectView = findViewById(R.id.editTextSubject);
-        TextView numberView = findViewById(R.id.editTextNumber);
+    public void onSaveHistoryButtonClicked(View view) throws NumberFormatException{
+        try {
+            TextView yearView = findViewById(R.id.editTextYear);
+            TextView quarterView = findViewById(R.id.editTextQuarter);
+            TextView subjectView = findViewById(R.id.editTextSubject);
+            TextView numberView = findViewById(R.id.editTextNumber);
 
-        String subjectAndNumber = subjectView.getText().toString() + numberView.getText().toString();
-        String quarter = quarterView.getText().toString();
-        int year = Integer.parseInt(yearView.getText().toString());
+            String subjectAndNumber = subjectView.getText().toString() + numberView.getText().toString();
+            String quarter = quarterView.getText().toString();
+            int year = Integer.parseInt(yearView.getText().toString());
 
-        Course newCourse = new Course(year, quarter, subjectAndNumber);
+            Course newCourse = new Course(year, quarter, subjectAndNumber);
+            try{
+                db.coursesDao().insert(newCourse);
+            }
+            catch (SQLiteConstraintException e){
+                Utilities.showAlert(this,"Class has already been entered");
+            }
 
-        db.coursesDao().insert(newCourse);
+
+            yearView.setText("");
+            quarterView.setText("");
+            subjectView.setText("");
+            numberView.setText("");
+        }
+        catch(NumberFormatException e){
+            Utilities.showAlert(this, "Year has to be a number");
+        }
+
+
+    }
+
+    public void onExitButtonClicked(View view){
+        finish();
     }
 }
