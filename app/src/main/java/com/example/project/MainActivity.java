012d4d83d294f71b23afd87877c7a1a1b704bf2c
w3yu <59678644+w3yu@users.noreply.github.com>
2022-03-01 19:03:05 -0800

    US1 testing incomplete

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
     SignInButton signInButton;
     private GoogleApiClient googleApiClient;
-    TextView textView;
+    //TextView textView;
     private static final int RC_SIGN_IN = 1;
 
 
@@ -266,8 +266,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
-
-        String type =adapter.toString();
     }
 
     public void onButtonOpenHistoryClicked(View view){
@@ -354,24 +352,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         switch (text)
         {
             case "Recency":
-                Collections.sort(fellowUsers, new Comparator<User>() {
-                    @Override
-                    public int compare(User user, User t1) {
-                        // compare year first
-                        int year = RecencyYearHelper(user.getCourses()) < RecencyYearHelper(t1.getCourses()) ? 1 :
-                                RecencyYearHelper(user.getCourses()) > RecencyYearHelper(t1.getCourses()) ? -1 : 0;
-
-                        // if there is a recency between year, return directly
-                        if (year != 0)
-                        {
-                            return year;
-                        }
-
-                        // if they have the same max year, compare quarter
-                        return RecencyQuarterHelper(user.getCourses()) < RecencyQuarterHelper(t1.getCourses()) ? 1 :
-                                RecencyQuarterHelper(user.getCourses()) > RecencyQuarterHelper(t1.getCourses()) ? -1 : 0;
-                    }
-                });
+                Collections.sort(fellowUsers, new SortByRecencyComparator());
         }
     }
 
@@ -380,75 +361,15 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
     }
 
-    // return the max year in a given list of courses
-    public int RecencyYearHelper(List<Course> list)
-    {
-        int max = list.get(0).getYear();
-        for(int i = 1; i < list.size(); i++)
-        {
-            if (list.get(i).getYear() > max)
-            {
-                max = list.get(i).getYear();
-            }
-        }
-        return max;
+
+    //Set method for BDD scenario test
+    public void setFellowUsers(List<User> fellowUsers){
+        this.fellowUsers = fellowUsers;
     }
 
-    // return the int value of each quarter base on recency:
-    // fall -> 6; sss -> 5; ... spring -> 2; winter -> 1
-    public int RecencyQuarterHelper(List<Course> list)
-    {
-        // temp var that stores the output
-        int count = 0;
-        int maxYear = RecencyYearHelper(list);
-
-        for(int i = 0; i < list.size(); i++)
-        {
-            // only compare the course with max year, neglect the rest
-            if (list.get(i).getYear() == maxYear)
-            {
-                String quarter = list.get(i).getQuarter();
-                if (quarter.equals("fall"))
-                {
-                    // if there is fall quarter, return directly
-                    return 6;
-                }
-                else if (quarter.equals("special summer session"))
-                {
-                    // if sss, assign count to 5
-                    count = 5;
-                }
-                else if (quarter.equals("summer session ii"))
-                {
-                    // only change value when quarter of lower recency is visited
-                    if (count < 4)
-                    {
-                        count = 4;
-                    }
-                }
-                else if (quarter.equals("summer session i"))
-                {
-                    if (count < 3)
-                    {
-                        count = 3;
-                    }
-                }
-                else if (quarter.equals("spring"))
-                {
-                    if (count < 2)
-                    {
-                        count = 2;
-                    }
-                }
-                else if (quarter.equals("winter"))
-                {
-                    if (count < 1)
-                    {
-                        count = 1;
-                    }
-                }
-            }
-        }
-        return count;
+    //Get method for BDD scenario test
+    public List<User> getFellowUsers(){
+        return this.fellowUsers;
     }
+
 }
--- app/src/test/java/test/Sort_by_recencyTest.java
+++ app/src/test/java/test/Sort_by_recencyTest.java
@@ -1,13 +1,69 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import androidx.lifecycle.Lifecycle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.project.MainActivity;
+import com.example.project.R;
+import com.example.project.SortByRecencyComparator;
+import com.example.project.UsersViewAdapter;
+import com.example.project.model.Course;
+import com.example.project.model.User;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-public class Sort_by_recencyTest {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+@RunWith(AndroidJUnit4.class)
+
+//Implement AdapterView to get the spinner, the object for dropdown menu
+public class Sort_by_recencyTest implements AdapterView.OnItemSelectedListener{
+
+    protected RecyclerView usersRecyclerView;
+    protected RecyclerView.LayoutManager usersLayoutManager;
+    protected UsersViewAdapter userViewAdapter;
+
+    List<User> fellowUsers = new ArrayList<User>();
+
+
+    @Before
+    public void setUp(){
+
+
+
+        Course demo1 = new Course(2020, "spring", "CSE110");
+        Course demo2 = new Course(2077, "winter", "CSE101");
+        Course demo3 = new Course(2020, "fall", "CSE2");
+
+        User user1 = new User("Luffy","",new ArrayList<Course>(), 17);
+        User user2 = new User("Zoro","",new ArrayList<Course>(), 20);
+        User user3 = new User("Nami","", new ArrayList<Course>(), 22);
+
+
+        user1.getCourses().add(demo1);
+        user2.getCourses().add(demo2);
+        user3.getCourses().add(demo3);
+
+        fellowUsers.add(user1);
+        fellowUsers.add(user2);
+        fellowUsers.add(user3);
+
+
+    }
+
 
     @Test
     public void sortedCorrectly(){
@@ -15,11 +71,44 @@ public class Sort_by_recencyTest {
         scenario.moveToState(Lifecycle.State.CREATED);
         scenario.onActivity(activity -> {
 
+            //TODO get users from recyclerView
+            activity.setFellowUsers(this.fellowUsers);
 
+            usersRecyclerView = activity.findViewById(R.id.users_view);
 
+            usersLayoutManager = new LinearLayoutManager(activity);
+            usersRecyclerView.setLayoutManager(usersLayoutManager);
 
+            userViewAdapter = new UsersViewAdapter(activity.getFellowUsers());
+            usersRecyclerView.setAdapter(userViewAdapter);
+
+            //TODO: perform click on recency in the dropdown menu
+            Spinner spinner = activity.findViewById(R.id.spinner);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(activity.getApplicationContext(),
+                    R.array.font_sizes, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+            spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
 
         });
+        assertEquals(1, 1);
+        assertEquals(fellowUsers.get(0).getName(), "Luffy");
+    }
+
+    // implementation of AdapterView
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        // Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+        switch (text)
+        {
+            case "Recency":
+                Collections.sort(fellowUsers, new SortByRecencyComparator());
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
